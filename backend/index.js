f65c4cb56c272f433028b9e1328acb283cb31144
Sarath Kammari <sarathkammari1987>
2022-03-03 21:23:59 +0530

    Configuration updated to support production release

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,12 @@ require("./startup/logging")();
 require("./startup/cors")(app);
 require("./startup/routes")(app);
 require("./startup/db")();
+require("./startup/prod")(app);
 
 const port = process.env.PORT || config.get("port");
-const server = app.listen(port, () =>
-  winston.info(`Listening on port ${port}...`)
-);
+const server = app.listen(port, () => {
+  winston.info(`App Env: ${app.get('env')}`);
+  winston.info(`Listening on port ${port}...`);
+});
 
 module.exports = server;
